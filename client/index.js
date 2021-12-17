83fef25704631415331365222921d3413269aff7
Ollie Turnbull <oturnbull1@gmail.com>
2021-12-17 16:14:42 +0000

    Completed project

--- client/index.js
+++ client/index.js
@@ -1,4 +1,7 @@
 import "./index.scss";
+const EC = require('elliptic').ec;
+const SHA256 = require('crypto-js/sha256');
+const ec = new EC('secp256k1');
 
 const server = "http://localhost:3042";
 
@@ -19,9 +22,14 @@ document.getElementById("transfer-amount").addEventListener('click', () => {
   const sender = document.getElementById("exchange-address").value;
   const amount = document.getElementById("send-amount").value;
   const recipient = document.getElementById("recipient").value;
-
+  // sign the message 
+  const key = ec.keyFromPrivate(document.getElementById("private-key").value);
+  const message = `${amount} to ${recipient}`;
+  const msgHash = SHA256(message);
+  const signature = key.sign(msgHash.toString());
+  
   const body = JSON.stringify({
-    sender, amount, recipient
+    sender, amount, recipient, signature
   });
 
   const request = new Request(`${server}/send`, { method: 'POST', body });
--- server/index.js
+++ server/index.js
@@ -1,17 +1,36 @@
 const express = require('express');
+const EC = require('elliptic').ec;
 const app = express();
 const cors = require('cors');
 const port = 3042;
+const SHA256 = require('crypto-js/sha256');
 
 // localhost can have cross origin errors
 // depending on the browser you use!
 app.use(cors());
 app.use(express.json());
 
+// generate the keys
+var ec = new EC('secp256k1');
+const keys = []
+for (let x = 0; x<3;x++) {
+  let key = ec.genKeyPair();
+  let publicStr = key.getPublic().encode('hex');
+  key.publicKey = publicStr;
+  key.privateKey = key.getPrivate().toString(16);
+
+  keys.push(key);
+}
+
+keys.forEach((key) => {
+  console.log(key.publicKey);
+  console.log(key.privateKey);
+});
+
 const balances = {
-  "1": 100,
-  "2": 50,
-  "3": 75,
+  [keys[0].publicKey] : 100,
+  [keys[1].publicKey]: 50,
+  [keys[2].publicKey]: 75,
 }
 
 app.get('/balance/:address', (req, res) => {
@@ -21,10 +40,20 @@ app.get('/balance/:address', (req, res) => {
 });
 
 app.post('/send', (req, res) => {
-  const {sender, recipient, amount} = req.body;
-  balances[sender] -= amount;
-  balances[recipient] = (balances[recipient] || 0) + +amount;
-  res.send({ balance: balances[sender] });
+  const {sender, recipient, amount, signature} = req.body;
+  // Check for signature
+  const key = ec.keyFromPublic(sender, 'hex');
+  const msg = `${amount} to ${recipient}`;
+  const msgHash = SHA256(msg).toString();
+  if (key.verify(msgHash, signature)){
+    console.log("Authorised transaction.");
+    balances[sender] -= amount;
+    balances[recipient] = (balances[recipient] || 0) + +amount;
+    res.send({ balance: balances[sender] });
+  }
+  else {
+    console.log("Incorrect signature, not authorised");
+  }
 });
 
 app.listen(port, () => {
